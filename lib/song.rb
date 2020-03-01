234889240b1925522e440b0624574b19627e2463
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-01 16:05:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,40 @@ class Song
     self.class.all << self
   end
 
+  def Song.create
+
+  end
+  
+  def Song.new_by_name
+    
+  end
+  
+  def Song.create_by_name
+    
+  end
+  
+  def Song.find_by_name
+    
+  end
+  
+  def Song.find_or_create_by_name
+    
+  end
+  
+  def Song.alphabetical
+    @@all.sort_by(&:alphabetical)
+  end
+  
+  def Song.new_from_filename
+    
+  end
+  
+  def Song.create_from_filename
+    
+  end
+  
+  def Song.destroy_all
+    @@all.clear
+  end
+
 end
