d13f7c358b8175310134f96d490ce4ed92254c6c
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-01 18:33:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   end
   
   def Song.find_or_create_by_name(song_name)
-    song.find_by_name || song.create_by_name
+    self.find_by_name || self.create_by_name
   end
   
   def Song.alphabetical
